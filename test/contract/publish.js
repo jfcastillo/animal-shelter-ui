70c7fc9dd079e4b17072231d78ddee72af4511aa
Felipe Castillo <jfcastillo1204@gmail.com>
2021-04-21 21:23:02 -0500

    publish error fixed

--- test/contract/publish.js
+++ test/contract/publish.js
@@ -4,9 +4,9 @@ dotenv.config();
 
 const opts = {
     pactBroker: process.env.PACT_BROKER_BASE_URL,
-    pactBrokerToker: process.env.PACT_BROKER_TOKEN,
+    pactBrokerToken: process.env.PACT_BROKER_TOKEN,
     consumerVersion: process.env.PACT_CONSUMER_VERSION,
-    pactFilesOrDirs: './test\contract\pacts\animalshelterfront-animalshelterback.json'
+    pactFilesOrDirs: ['./test/contract/pacts/animalshelterfront-animalshelterback.json']
 }
 
 new Publisher(opts).publishPacts();
